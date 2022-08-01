5e2398813463526500c8d55be3ea9dc0d3dcd680
clausserg <claudiu.sergentu@gmail.com>
2022-08-01 14:59:51 +0300

    added RasOrb file to begin testing and impl of the main module

--- morot/morot.py
+++ morot/morot.py
@@ -1,5 +1,15 @@
-"""Provide the primary functions."""
+"""Main module.
+
+Python code to rotate by user-defined angles, two-by-two, molecular orbitals produced by
+the RASSCF module of the OpenMOlcas quantum chemistry code.
+"""
+
+from orbitals_class import MolcasOrbitals
 
 
 if __name__ == "__main__":
-    pass
+    mo_output_file = "./data/orbitals.RasOrb"
+    myorbitals = []
+    with open(mo_output_file, 'r') as rfile:
+        myorbitals = rfile.readlines()
+

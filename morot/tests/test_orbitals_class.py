bcad6c69f896bffe40710e827cca33938d2465e8
clausserg <claudiu.sergentu@gmail.com>
2022-08-05 12:20:18 +0300

    added a test module

---
+++ morot/tests/test_orbitals_class.py
@@ -0,0 +1,19 @@
+"""
+Unit and regression test for the morot package.
+"""
+
+# Import package, test suite, and other packages as needed
+from morot.orbitals_class import MolcasOrbitals
+
+
+def test_instance_attributes():
+    """
+    Testing some instance variables
+    """
+    input_mos = "../data/orbitals.RasOrb"
+    MOs = MolcasOrbitals(input_mos)
+    assert MOs.in_orb_file == input_mos and MOs.out_orb_file == "orbitals.RotOrb" and \
+        len(MOs.irreps) == 8 and len(MOs.orbs_per_irrep) == 8 and \
+            len(MOs.orbitals) == 710
+
+

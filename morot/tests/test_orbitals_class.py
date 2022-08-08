ce55c61ba111c2ac46e6c3b7af953179ffc5e9a5
clausserg <claudiu.sergentu@gmail.com>
2022-08-08 16:12:20 +0300

    added module to test instance variables with an orbital set without symmetry

--- morot/tests/test_orbitals_class.py
+++ morot/tests/test_orbitals_class.py
@@ -6,9 +6,9 @@ Unit and regression test for the morot package.
 from morot.orbitals_class import MolcasOrbitals
 
 
-def test_instance_attributes():
+def test_instance_attributes_sym_d2h():
     """
-    Testing some instance variables
+    Testing some instance variables with orbitals obtained for berkelocene, symmetry D2h
     """
     input_mos = "../data/orbitals.RasOrb"
     MOs = MolcasOrbitals(input_mos)
@@ -17,4 +17,13 @@ def test_instance_attributes():
             len(MOs.orbitals) == 710 and len(MOs.header) == 11 and \
                 len(MOs.footer) == 388
 
-
+def test_instance_attributes_sym_c1():
+    """
+    Testing some instance variables with orbitals obtained for molecular oxygen, symmetry=nosym
+    """
+    input_mos = "../data/dioxygen.RasOrb"
+    MOs = MolcasOrbitals(input_mos)
+    assert MOs.in_orb_file == input_mos and MOs.out_orb_file == "dioxygen.RotOrb" and \
+        len(MOs.irreps) == 1 and len(MOs.orbs_per_irrep) == 1 and \
+            len(MOs.orbitals) == 46 and len(MOs.header) == 11 and \
+                len(MOs.footer) == 33

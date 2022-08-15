de29ccd478d447834026fde12264be8dcb146cac
Dumitru-Claudiu <claudiu.sergentu@gmail.com>
2022-08-15 07:27:43 +0300

    added test module for the class len dunder method

--- morot/tests/test_orbitals_class.py
+++ morot/tests/test_orbitals_class.py
@@ -28,7 +28,7 @@ def test_instance_attributes_sym_c1():
             len(MOs.orbitals) == 46 and len(MOs.header) == 11 and \
                 len(MOs.footer) == 33
 
-def test_class_str_dunder_d2h():
+def test_str_dunder_d2h():
     """
     Testing the __str__() dunder method
     """
@@ -36,3 +36,18 @@ def test_class_str_dunder_d2h():
     MOs = MolcasOrbitals(input_mos) 
     assert MOs.__str__() == "710 molecular orbitals spanning 8 irreps"
 
+def test_len_dunder_d2h():
+    """
+    Testing the __len__() dunder method with D2h-symmetry orbitals
+    """
+    input_mos = "../data/orbitals.RasOrb"
+    MOs = MolcasOrbitals(input_mos) 
+    assert MOs.__len__() == 710
+
+def test_len_dunder_c1():
+    """
+    Testing the __len__() dunder method with orbitals w/o symmetry
+    """
+    input_mos = "../data/dioxygen.RasOrb"
+    MOs = MolcasOrbitals(input_mos) 
+    assert MOs.__len__() == 46

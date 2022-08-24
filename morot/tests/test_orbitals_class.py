6b658d53e6f7bf4fede982c6dc17f5d39b3610a0
clausserg <claudiu.sergentu@gmail.com>
2022-08-24 11:50:55 +0300

    updated paths to files to get CI passing

--- morot/tests/test_orbitals_class.py
+++ morot/tests/test_orbitals_class.py
@@ -10,7 +10,7 @@ def test_instance_attributes_sym_d2h():
     """
     Testing some instance variables with orbitals obtained for berkelocene, symmetry=D2h
     """
-    input_mos = "../data/orbitals.RasOrb"
+    input_mos = "morot/data/orbitals.RasOrb"
     MOs = MolcasOrbitals(input_mos)
     assert MOs.in_orb_file == input_mos and MOs.out_orb_file == "orbitals.RotOrb" and \
         len(MOs.irreps) == 8 and len(MOs.orbs_per_irrep) == 8 and \
@@ -21,7 +21,7 @@ def test_instance_attributes_sym_c1():
     """
     Testing some instance variables with orbitals obtained for molecular oxygen, symmetry=nosym
     """
-    input_mos = "../data/dioxygen.RasOrb"
+    input_mos = "morot/data/dioxygen.RasOrb"
     MOs = MolcasOrbitals(input_mos)
     assert MOs.in_orb_file == input_mos and MOs.out_orb_file == "dioxygen.RotOrb" and \
         len(MOs.irreps) == 1 and len(MOs.orbs_per_irrep) == 1 and \
@@ -32,7 +32,7 @@ def test_str_dunder_d2h():
     """
     Testing the __str__() dunder method
     """
-    input_mos = "../data/orbitals.RasOrb"
+    input_mos = "morot/data/orbitals.RasOrb"
     MOs = MolcasOrbitals(input_mos) 
     assert MOs.__str__() == "710 molecular orbitals spanning 8 irreps"
 
@@ -40,7 +40,7 @@ def test_len_dunder_d2h():
     """
     Testing the __len__() dunder method with D2h-symmetry orbitals
     """
-    input_mos = "../data/orbitals.RasOrb"
+    input_mos = "morot/data/orbitals.RasOrb"
     MOs = MolcasOrbitals(input_mos) 
     assert MOs.__len__() == 710
 
@@ -48,6 +48,6 @@ def test_len_dunder_c1():
     """
     Testing the __len__() dunder method with orbitals w/o symmetry
     """
-    input_mos = "../data/dioxygen.RasOrb"
+    input_mos = "morot/data/dioxygen.RasOrb"
     MOs = MolcasOrbitals(input_mos) 
     assert MOs.__len__() == 46
